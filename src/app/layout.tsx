8796a68ef54a92856803e7c5136a32decb943791
Lucas <lucas.jd07.goncalves@gmail.com>
2025-03-03 16:10:57 -0300

    feat: add initial cart context and card sheets components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,21 @@
+"use client";
+
 import "./globals.css";
 
-import type { Metadata } from "next";
+// import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
+
+import { CartProvider } from "./[slug]/menu/contexts/cart";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "FSW Donalds",
-  description: "Bora finalizar esse projeto lindo!",
-};
+// export const metadata: Metadata = {
+//   title: "FSW Donalds",
+//   description: "Bora finalizar esse projeto lindo!",
+// };
 
 export default function RootLayout({
   children,
@@ -20,7 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} antialiased`}>{children}</body>
+      <body className={`${poppins.className} antialiased`}>
+        <CartProvider>{children}</CartProvider>
+        
+      </body>
     </html>
   );
 }
